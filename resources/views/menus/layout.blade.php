515309af2bee7261cc82ffb327bf7904256f4554
KIRAGGA <preagkan@gmail.com>
2020-06-29 23:14:52 +0300

    Working on the layout

--- resources/views/menus/layout.blade.php
+++ resources/views/menus/layout.blade.php
@@ -1,60 +1,154 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>School</title>
+<title>CSS Template</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Style the header */
+.header {
+  background-color: #f1f1f1;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+}
+
+/* Container for flexboxes */
+.row {
+  display: -webkit-flex;
+  display: flex;
+}
+
+/* Create three equal columns that sits next to each other */
+.column {
+  -webkit-flex: 1;
+  -ms-flex: 1;
+  flex: 1;
+  padding: 10px;
+  height: 300px; /* Should be removed. Only for demonstration */
+}
+
+/* Style the footer */
+.footer {
+  background-color: #f1f1f1;
+  padding: 10px;
+  text-align: center;
+}
+
+.left,
+.middle,
+.right {
+  padding: 10px;
+  height: 300px; /* Should be removed. Only for demonstration */
+}
+
+
+.left {
+  grid-area: left;
+}
+
+/* Style the middle column */
+.middle {
+  grid-area: middle;
+}
+
+/* Style the right column */
+.right {
+  grid-area: right;
+}
+
+/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+@media (max-width: 600px) {
+  .row {
+    -webkit-flex-direction: column;
+    flex-direction: column;
+  }
+}
+</style>
 </head>
 <body>
-    <div>
-        <div>
-            <div class="flex-center position-ref full-height">
-                <ul class="social">
-                    <li>
-                        <a href="https" title="facebook" target="_blank">face</a>
-                    </li>
-                    <li>
-                        <a href="https" title="twitter" target="_blank">twitter</a>
-                    </li>
-                    <li>
-                        <a href="https" title="youtube" target="_blank">youtube</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#services">SERVICES</a></li>
+        <li><a href="#portfolio">PORTFOLIO</a></li>
+        <li><a href="#pricing">PRICING</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+@yield('content')
+
+
+
+
+
+
+
+  
+<div class="container text-center">    
+  <h3>What We Do</h3><br>
+  <div class="row">
+    <div class="col-sm-3">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Current Project</p>
+    </div>
+    <div class="col-sm-3"> 
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Project 2</p>    
     </div>
     
-    @yield('content')
+    <div class="col-sm-3">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Project 3</p> 
+     </div>
+     
+     <div class="col-sm-3">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <p>Project 4</p> 
+     </div>
+    
+  </div>
+</div><br>
 
-    <footer>
-        <div>
-            <div>
-                <div>
-                    <aside><h3 class="widget-title">About Us</h3>
-                        <div> <p style="text-align: justify;"> Lungujja Model nursery and Primary school is a private
-                        modern day and boarding school. The school started in. Lungujja Model nursery and Primary school is a private
-                        modern day and boarding school. The school started in. Lungujja Model nursery and Primary school is a private
-                        modern day and boarding school. The school started in the year 2000</p></div>
-                    </aside>
-                    <aside>
-                        <h3 class="widget-title"></h3> 
-                        <div>
-                            <iframe src="insert google map here" frameborder="0" style="border: 0;" allowfullscreen></iframe>
-                        </div>
-                    </aside>
-                    <aside>
-                        <h3 class="widget-title">Contact Details</h3>
-                        <div>
-                            <p> Lungujja Model Primary School</p>
-                            <p>P.O Box 7888</p>
-                            <p>Phone No: 77812354</p>
-                            <p>Email: info@jkldsdijldasdc.co.ug</p>
-                        </div>
-                    </aside>
-                </div>
-            </div>
-        </div>
-    </footer>
+
+
+<div class="row">
+  <div class="column" style="background-color:#ccc;">Column</div>
+  <div class="column" style="background-color:#ccc;">Column</div>
+  <div class="column" style="background-color:#ccc;">Column</div>
+</div>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
+
 </body>
 </html>
