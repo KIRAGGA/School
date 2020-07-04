b28627dbb8a30c47e55870b57de22161b64ad165
KIRAGGA <preagkan@gmail.com>
2020-07-05 00:40:10 +0300

    working on menus

--- resources/views/menus/AboutUs.blade.php
+++ resources/views/menus/AboutUs.blade.php
@@ -1,6 +1,11 @@
 @extends('menus.layout')
 
 @section('content')
+
+
+    <header style=" background-color: #666; padding: 30px ; text-align: left; font-size: 35px; color: white;">
+        <p>About the School</p>
+    </header>
 <aside style="-webkit-flex: 3; -ms-flex: 3; flex: 3; background-color: #f1f1f1; padding: 10px;">
     <h1>MOTTO: INTEGRITY AND KNOWLEDGE</h1>
 
