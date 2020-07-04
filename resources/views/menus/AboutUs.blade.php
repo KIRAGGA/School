02b4d8e005ab54ce77ebc1bb91e8e0989cc50f9a
KIRAGGA <preagkan@gmail.com>
2020-07-04 23:47:17 +0300

    working on menus

---
+++ resources/views/menus/AboutUs.blade.php
@@ -0,0 +1,17 @@
+@extends('menus.layout')
+
+@section('content')
+<aside style="-webkit-flex: 3; -ms-flex: 3; flex: 3; background-color: #f1f1f1; padding: 10px;">
+    <h1>MOTTO: INTEGRITY AND KNOWLEDGE</h1>
+
+<p>St. George’s Girls’ Secondary School is a Public Provincial Girls’ Boarding School in Nairobi Province. 
+        The school started in 1986 as a Mixed Day School but later converted to a Girls’ 
+        Boarding School when the boys were relocated to Nairobi Milimani Secondary School. 
+        The school population stands at 1190 girls currently with 6 classes each. Our girls are mainly from 
+        Nairobi County but about one tenth of them are picked from the other 46 Counties. 
+        We are also hosting the UNICEF girls and Affirmative Action girls from marginalized districts. 
+        Our performance through the years has been very good in the KCSE KNEC Examinations.
+</p>
+</aside>
+    
+@endsection
