47019d810334df9401bca4f5d239e6e3261cc2a6
KIRAGGA <preagkan@gmail.com>
2020-07-24 13:55:17 +0300

    Working on Staff

---
+++ resources/views/menus/Departments.blade.php
@@ -0,0 +1,11 @@
+@extends('menus.layout')
+
+@section('content')
+
+<header style=" background-color: #666; padding: 30px ; text-align: left; font-size: 35px; grid-area: header; color: white;">
+    <h1>Departments</h1>
+</header>
+
+
+    
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,8 +64,8 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+        {{-- <div class="flex-center position-ref full-height"> --}}
+            {{-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -77,24 +77,50 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif --}}
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            @extends('menus.layout')
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                @section('content')
+
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="https://placehold.it/1200x400?text=IMAGE" alt="Image">
+                        <div class="carousel-caption">
+                        <h3>School</h3>
+                        <p>Administration</p>
+                        </div>      
+                    </div>
+
+                    <div class="item">
+                        <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="Image">
+                        <div class="carousel-caption">
+                        <h3>Sch</h3>
+                        <p>thiss.</p>
+                        </div>      
+                    </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                    </a>
                 </div>
-            </div>
-        </div>
+                    
+                @endsection
+        {{-- </div> --}}
     </body>
 </html>
