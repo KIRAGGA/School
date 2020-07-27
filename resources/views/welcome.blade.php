3753fda058e9481340de0f3b2832498c4b389f33
KIRAGGA <preagkan@gmail.com>
2020-07-27 20:15:48 +0300

    working on Contact

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -131,13 +131,21 @@
                     </a>
                 </div>
 
-{{-- Second div --}}
-                <div>
+{{-- Second div about welcoming --}}
+                <div><br>
+                    <h2 style="text-align: center">Welcome to This is a Secondary School</h2>
                     <p style="float: left; padding: 15px;"><img src="{{URL::asset('/storage/images/capture.png')}}" 
                         alt="school" style="border: 1px solid #ddd; border-radius: 2px; padding: 5px; width: 276px;" class="w3-hover-opacity"></p><br>
-                    <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-                      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                      laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p >This is a Secondary School is a Public Provincial Girls’ Boarding School in Nairobi Province. 
+                        The school started in 1986 as a Mixed Day School but later converted to a Girls’ Boarding School when the boys were 
+                        relocated to Nairobi Milimani Secondary School.
+
+                    </p>
+                    <p>The school population stands at 984 girls currently with 5 classes each. Our girls are mainly from 
+                        Nairobi Province but about one tenth of them are picked from the other 7 provinces. 
+                        We are also hosting the UNICEF girls and Affirmative Action girls from marginalized districts. 
+                        Our performance through the years has been very good in the KCSE KNEC Examinations.
+                    </p>
                 </div>
 
                 {{-- the vision, mission and motto of the school --}}
