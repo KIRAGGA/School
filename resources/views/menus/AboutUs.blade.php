c3153e8f10d6a2eea7155cf606b62696369ff97d
KIRAGGA <preagkan@gmail.com>
2020-07-28 12:41:37 +0300

    working on schoolfaclities

--- resources/views/menus/AboutUs.blade.php
+++ resources/views/menus/AboutUs.blade.php
@@ -9,21 +9,42 @@
 
 
 <div>
+    
     <h2 style="text-align: center">Welcome to This is a Secondary School</h2>
     <p style="float: left; padding: 15px;"><img src="{{URL::asset('/storage/images/capture.png')}}" 
         alt="school" style="border: 1px solid #ddd; border-radius: 2px; padding: 5px; width: 276px;" class="w3-hover-opacity"></p><br>
-    <p >This is a Secondary School is a Public Provincial Girls’ Boarding School in Nairobi Province. 
-        The school started in 1986 as a Mixed Day School but later converted to a Girls’ Boarding School when the boys were 
-        relocated to Nairobi Milimani Secondary School.
+    <p >St. George’s Girls’ Secondary School is a Public Provincial Girls’ Boarding School in Nairobi Province. 
+        The school started in 1986 as a Mixed Day School but later converted to a Girls’ Boarding School when the 
+        boys were relocated to Nairobi Milimani Secondary School. The school population stands at 1190 girls currently 
+        with 6 classes each. Our girls are mainly from Nairobi County but about one tenth of them are picked from the 
+        other 46 Counties. We are also hosting the UNICEF girls and Affirmative Action girls
+         from marginalized districts. Our performance through the years has been very good in the KCSE KNEC Examinations.
 
     </p>
-    <p>The school population stands at 984 girls currently with 5 classes each. Our girls are mainly from 
-        Nairobi Province but about one tenth of them are picked from the other 7 provinces. 
-        We are also hosting the UNICEF girls and Affirmative Action girls from marginalized districts. 
-        Our performance through the years has been very good in the KCSE KNEC Examinations.
-    </p>
-</div><br><hr>
-
+   
+</div><br>
+<div class="rows" style="align-items: center">
+    <br><hr>
+    <div class="column" style="text-align: center">
+        
+        <h3>Our Vision</h3>
+        <p>Our School Vision is to Be A Center of Quality Education and empower Members to greater Heights</p>
+    </div>
+    
+  
+    <div class="column">
+        
+        <h3>Our Vision</h3>
+        <p>Our School Vision is to Be A Center of Quality Education and empower Members to greater Heights</p>
+    </div>
+  
+    <div class="column">
+        
+        <h3>Our Motto</h3>
+        <p>Our School Vision is to Be A Center of Quality Education and empower Members to greater Heights</p>
+    </div>
+  
+</div>
 
     
 @endsection
