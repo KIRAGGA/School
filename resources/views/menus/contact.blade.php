ff6932a0f6dd2042184dc95bbfd3e585dee068c0
KIRAGGA <preagkan@gmail.com>
2020-08-03 23:18:33 +0300

    working on contact

--- resources/views/menus/contact.blade.php
+++ resources/views/menus/contact.blade.php
@@ -89,7 +89,7 @@
                 </div>
                 <div class="row">
                  <div class="update ml-auto mr-auto">
-                    <button type="submit" class="btn btn-primary btn-round">Send</button>
+                  <button type="submit" class="w3-button w3-black w3-margin-bottom"><i class="fa fa-paper-plane w3-margin-right"></i>Send Message</button>
                   </div>
                 </div>
               </form>
@@ -99,27 +99,6 @@
       </div>
    </div>
 
-
-
-
-
-
-
-    <form action="/action_page.php" target="_blank">
-      <div class="w3-section">
-        <label>Name</label>
-        <input class="w3-input w3-border" type="text" name="Name" required>
-      </div>
-      <div class="w3-section">
-        <label>Email</label>
-        <input class="w3-input w3-border" type="text" name="Email" required>
-      </div>
-      <div class="w3-section">
-        <label>Message</label>
-        <input class="w3-input w3-border" type="text" name="Message" required>
-      </div>
-      <button type="submit" class="w3-button w3-black w3-margin-bottom"><i class="fa fa-paper-plane w3-margin-right"></i>Send Message</button>
-    </form>
   </div><br>
 
 @endsection
