6b96fa4e4ffba58ea31c9d144e4a00c81574db30
KIRAGGA <preagkan@gmail.com>
2020-08-03 18:20:38 +0300

    working on contact

--- resources/views/menus/contact.blade.php
+++ resources/views/menus/contact.blade.php
@@ -4,11 +4,10 @@
     
 <header style=" background-color: #666; padding: 10px ; text-align: left; font-size: 35px; grid-area: header; color: white; height: 100px">
     <h1 style="height: 100px; width: 244px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact US</h1>
-</header>
+</header><br><br>
 
  <!-- Contact Section -->
  <div class="w3-container w3-padding-large w3-grey">
-    <h4 id="contact"><b>Contact Me</b></h4>
     <div class="w3-row-padding w3-center w3-padding-24" style="margin:0 -16px">
       <div class="w3-third w3-dark-grey">
         <p><i class="fa fa-envelope w3-xxlarge w3-text-light-grey"></i></p>
@@ -18,7 +17,7 @@
         <p><i class="fa fa-map-marker w3-xxlarge w3-text-light-grey"></i></p>
         <p>Nairobi, Kenya</p>
       </div>
-      <div class="w3-third w3-dark-grey">
+      <div class="w3-third w3-orange">
         <p><i class="fa fa-phone w3-xxlarge w3-text-light-grey"></i></p>
         <p>+254711111010</p>
       </div>
@@ -39,6 +38,6 @@
       </div>
       <button type="submit" class="w3-button w3-black w3-margin-bottom"><i class="fa fa-paper-plane w3-margin-right"></i>Send Message</button>
     </form>
-  </div>
+  </div><br>
 
 @endsection
