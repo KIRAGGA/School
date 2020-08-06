af68698ccf32cc9b274b925d15e672b9f983ee98
KIRAGGA <preagkan@gmail.com>
2020-08-06 16:38:38 +0300

    polishing the project

--- resources/views/menus/admission-form.blade.php
+++ resources/views/menus/admission-form.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-<header style=" background-color: #666; padding: 10px ; text-align: left; font-size: 35px; grid-area: header; color: white; height: 100px">
-    <h1 style="height: 100px; width: 244px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Admission Form</h1>
-  </header>
+<div class="topnav" style=" background-color: #665; padding: 55px ; text-align: left; font-size: 35px; grid-area: header; color: white; height: 130px">
+    <h1 style="height: 100px; width: 244px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Admission form</h1>
+</div>
     
   <p>Students can apply for admission in our school, by filling in the form below: </p>
 <div>
